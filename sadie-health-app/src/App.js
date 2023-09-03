2b4bdb96e6000d21ed25534a75ae277636fc6de7
44thDragon <134791038+44thDragon@users.noreply.github.com>
2023-09-03 12:46:37 -0700

    homepage added

--- sadie-health-app/src/App.js
+++ sadie-health-app/src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Validate and process the input value (e.g., check if it's between 1 and 900)
+    if (inputValue >= 1 && inputValue <= 900) {
+      alert(`You entered: ${inputValue}`);
+    } else {
+      alert('Please enter a number between 1 and 900.');
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello, World!</h1>
+        <p>Welcome to my React.js app on sadie.io</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter a number between 1 and 900:
+            <input
+              type="number"
+              value={inputValue}
+              onChange={handleInputChange}
+              min={1}
+              max={900}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
       </header>
     </div>
   );
